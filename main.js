5f830c8ccbbf1931108be56788dd034023208fbd
Andrew Gibson <andrewgib14@gmail.com>
2017-08-08 16:35:49 -0500

    added images and styling to blocks

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 let url = "http://recipepuppyproxy.herokuapp.com/api/?q=";
 
 let input = document.querySelector("input");
+let grid = document.querySelector(".grid");
 
 input.addEventListener("keypress", function (e) {
     if (e.keyCode === 13) {
-        searchText = e.target.value;
+        grid.innerHTML = "";
+        let searchText = e.target.value;
         url += searchText;
         axios.get(url).then(function (response) {
-            console.log(response.data);
+            let results = response.data.results;
+            for (let i = 0; i < results.length; i++) {
+                let thumbnail = results[i].thumbnail;
+                let currentTitle = results[i].title;
+                let currentLink = results[i].href;
+                if (thumbnail === "") {
+                    thumbnail = "img/fork-img.jpg"
+                }
+                grid.innerHTML += `<div class="recipe" style="background-image: url(${thumbnail})"><p><span>Dish:</span> <a href=${currentLink}>${currentTitle}</a></p></div>`
+
+                url = "http://recipepuppyproxy.herokuapp.com/api/?q=";
+            }
         })
 
     }
-    for (let i = 0; i < data.length; i++) {
 
-    }
 })
